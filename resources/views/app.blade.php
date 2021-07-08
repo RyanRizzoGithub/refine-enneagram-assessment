69bb12c4136ceba5fe7c9f781c833e9168bdae03
Riley Schriner <rtschriner@gmail.com>
2021-07-08 14:12:53 -0400

    feat(ui): update images and styles

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,8 +11,9 @@
         <link rel="icon" sizes="192x192" href="favicon.png">
 
         {{-- Fonts --}}
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;900&family=Oswald:wght@700&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@700&family=Open+Sans:ital@0;1&display=swap" rel="stylesheet">
 
         {{-- Styles --}}
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
